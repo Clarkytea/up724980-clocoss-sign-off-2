0b2eff320d1f8a9d735dacb5b749bae3a4ebdee2
rglc12 <rglc12@gmail.com>
2017-12-05 14:48:21 +0000

    updated db-datastore.js

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // todo: the namespace should be in a config file
-const datastore = require('@google-cloud/datastore')({ namespace: 'jacek' });
+const datastore = require('@google-cloud/datastore')({ namespace: 'ryan-lc-db' });
 
 function key(id) {
   return datastore.key(['strings', id]);
