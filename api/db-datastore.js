87d540d61687e54c11aae64af98cfc890e8c1399
rglc12 <rglc12@gmail.com>
2017-12-09 17:30:53 +0000

    update db-datastore.js

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -15,7 +15,7 @@ module.exports.list = async () => {
 
 module.exports.get = async (id) => {
   const [data] = await datastore.get(key(id));
-  if (data && data.val) return data.val;
+  if (data && data.val) return `${data.val}`;
   return '0';
 };
 
