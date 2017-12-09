b54f295cf77417ac8b0524e159c63900f430a360
rglc12 <rglc12@gmail.com>
2017-12-09 17:49:01 +0000

    Final update index.js?

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 // get the database model, use `export DBTYPE=inmemory` to use the in-memory model
 const modelType = process.env.DBTYPE || 'datastore';
-const db = require(`./db-datastore`);
+const db = require(`./db-datastore`); // Require the datastore API
 
 const api = express.Router();
 
@@ -18,6 +18,7 @@ api.get('/', async (req, res) => {
   }
 });
 
+// Get the value of an ID
 api.get('/:id(\\w+)', async (req, res) => {
   try {
     res.send(await db.get(req.params.id));
@@ -27,30 +28,30 @@ api.get('/:id(\\w+)', async (req, res) => {
   }
 });
 
+// Send the data (KeyID and its value) that will overwrite the existing value
 api.put('/:id(\\w+)', bodyParser.text(), async (req, res) => {
   try {
     res.send(await db.put(req.params.id, req.body));
-    //res.sendStatus(204);
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
   }
 });
 
+// Send the data entry (KeyID and its value) that will be added to the datastore
 api.post('/:id(\\w+)', bodyParser.text(), async (req, res) => {
   try {
     res.send(await db.post(req.params.id, req.body));
-    //res.sendStatus(204);
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
   }
 });
 
+// Delete the datastore entry with the same parsed ID
 api.delete('/:id(\\w+)', async (req, res) => {
   try {
     res.send(await db.delete(req.params.id), 204);
-    //res.sendStatus(204);
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
